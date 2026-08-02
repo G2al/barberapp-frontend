e30d2d4edb5aafc36db15a310f70fa12ecb90957
G2al <g2aluigi@gmail.com>
2026-08-02 12:45:14 +0200

    Configure Next.js startup for Plesk

---
+++ app.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { createServer } = require("node:http");
+const next = require("next");
+
+const port = Number.parseInt(process.env.PORT || "3000", 10);
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const application = next({
+  dev: false,
+  hostname,
+  port,
+});
+const handle = application.getRequestHandler();
+
+application
+  .prepare()
+  .then(() => {
+    createServer((request, response) => handle(request, response)).listen(
+      port,
+      hostname,
+      () => console.log(`BarberApp ready on port ${port}`),
+    );
+  })
+  .catch((error) => {
+    console.error("Unable to start BarberApp", error);
+    process.exit(1);
+  });
